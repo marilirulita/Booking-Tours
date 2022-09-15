51ec58c959dd3ebb668bfbf27026c2377d937b28
mar <marilirulita@gmail.com>
2022-09-15 10:53:07 -0700

    add routs anidation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :tours
-  resources :user_tours
+  resources :tours do
+    resources :user_tours
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
