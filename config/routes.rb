fb47d844980efd139b7c2fac6d68b2659c0e95fa
mar <marilirulita@gmail.com>
2022-09-15 10:17:32 -0700

    add routes for tours and userTours

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tours
+  resources :user_tours
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
