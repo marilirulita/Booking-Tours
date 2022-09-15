62d98c38aa076be8e8f7de993a7ab94d3fa81add
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-16 00:02:09 +0200

    Add unit testing for models

---
+++ spec/models/tour_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe Tour, type: :model do
+  context 'Testing Tour validations and methods' do
+    before :all do
+      @user = User.new(name: 'Mahmoud')
+      @user.save
+      @tour = Tour.create(title: "First Tour", description: "In this tour we will visit x and ...", user_id: @user.id, cost: 500)
+    end
+
+    after :all do
+      User.destroy_all
+      Tour.destroy_all
+    end
+
+    it 'title should be present' do
+      @tour.title = nil
+      expect(@tour).to_not be_valid
+    end
+
+    it 'title must not exceed 100 characters' do
+      @tour.title = 'h' * 101
+      expect(@tour).to_not be_valid
+    end
+
+    it 'description should be present' do
+      @tour.description = nil
+      expect(@tour).to_not be_valid
+    end
+
+    it 'description must not exceed 500 characters' do
+      @tour.description = 'h' * 501
+      expect(@tour).to_not be_valid
+    end
+
+    it 'cost should be present' do
+      @tour.cost = nil
+      expect(@tour).to_not be_valid
+    end
+
+    it 'cost should be a number' do
+      @tour.cost = "Ten"
+      expect(@tour).to_not be_valid
+    end
+
+    it 'cost should be non-negative number' do
+      @tour.cost = -4
+      expect(@tour).to_not be_valid
+    end
+
+    it 'user_id should be present' do
+      @tour.user_id = nil
+      expect(@tour).to_not be_valid
+    end
+  end
+end
