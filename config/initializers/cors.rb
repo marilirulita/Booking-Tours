7894cee198b8d6507d3a5bc7799cf68e7b88e75c
Santiago <alexander.cardenas10@outlook.com>
2022-09-16 10:36:56 -0500

    Add delete cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
-      resource '*', headers: :any, methods: [:get, :post]
+      resource '*', headers: :any, methods: [:get, :post, :delete]
     end
   end
   
